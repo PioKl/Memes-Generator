519e3a13cf30f00592928ec8926d99a41a5503d8
PioKl <kosiu342@gmail.com>
2020-06-30 19:49:30 +0200

    App.js|MemeList is in App

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../style/App.scss';
+import MemesList from './MemesList';
+
+function App() {
+  return (
+    <div className="App">
+      <MemesList />
+    </div>
+  );
+}
+
+export default App;
