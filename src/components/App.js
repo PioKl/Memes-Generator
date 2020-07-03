b5873e64519fa3fefdf999fa435238c924655587
PioKl <kosiu342@gmail.com>
2020-07-03 22:27:56 +0200

    App.js|Add Routing, ChosedMeme is now in App instead in MemesList (new method of implementation), useEffect is also now in App

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../style/App.scss';
 import MemesList from './MemesList';
+import ChosedMeme from './ChosedMeme';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import axios from 'axios';
 
 function App() {
+
+  const [memes, setMemes] = useState([]);
+  useEffect(() => {
+    axios
+      .get('https://api.imgflip.com/get_memes')
+      .then(response => {
+        console.log(response.data.data);
+        setMemes(response.data.data.memes);
+      })
+      .catch(error => {
+        console.log(error)
+      })
+
+  }, [])
+
+  const chosedMeme = memes.map(meme => (
+    <Route key={meme.id} exact path={`/${meme.name}`}>
+      <ChosedMeme pickedMeme={meme} />
+    </Route>
+  ))
   return (
-    <div className="App">
-      <MemesList />
-    </div>
+    <Router basename={process.env.PUBLIC_URL}>
+      <div className="App">
+        <Switch>
+          <Route exact path="/">
+            <MemesList tableOfMemes={memes} />
+          </Route>
+          {chosedMeme}
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
