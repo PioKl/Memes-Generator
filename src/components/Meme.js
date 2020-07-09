3bf37a12d02f14d596897a9c24f1c938948f673d
PioKl <kosiu342@gmail.com>
2020-07-09 20:08:54 +0200

    Meme.js|Add new classes

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import '../style/Meme.scss'
 
 const Meme = ({ meme }) => {
 
     return (
-        <img src={meme.url} width="225" height="225" title={meme.name} alt={meme.name} />
+        <div className="meme-card">
+            <img className="meme-card__img" src={meme.url} title={meme.name} alt={meme.name} />
+            <div className="meme-card__overlay">
+                <div className="meme-card__text">{meme.name}</div>
+            </div>
+        </div>
     );
 }
 
