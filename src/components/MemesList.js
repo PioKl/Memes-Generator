3bbcd08f6c283a4f2cedf35f6b84092b55308eec
PioKl <kosiu342@gmail.com>
2020-07-09 20:10:18 +0200

    MemesList.js|Add new classes

--- src/components/MemesList.js
+++ src/components/MemesList.js
@@ -2,12 +2,13 @@ import React from 'react';
 import Meme from './Meme';
 import { NavLink } from 'react-router-dom';
 import { whiteSpacesReplace } from '../functions/replace';
+import '../style/MemesList.scss';
 
 const MemesList = ({ tableOfMemes }) => {
 
     return (
 
-        <div>
+        <div className="memes-container">
             {tableOfMemes.map(meme => (
                 <NavLink key={meme.id} to={whiteSpacesReplace(meme.name)}>
                     <Meme meme={meme} key={meme.id} />
