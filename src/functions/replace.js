19ecff9e393fb57aa43a7eff3f7ac8f5787cf64b
PioKl <kosiu342@gmail.com>
2020-07-11 20:33:19 +0200

    replace.js|Now it will replace also "?"

--- src/functions/replace.js
+++ src/functions/replace.js
@@ -1,3 +1,3 @@
 export const whiteSpacesReplace = (string) => {
-    return string.replace(/\s/g, '');
+    return string.replace(/\s|\?/g, ''); //usuwa również znak zapytania - tak wygladaloby bez skrocenia return string.replace(/\s//g, '').replace(/\?/g, '')
 }
