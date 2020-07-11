f356ffc9a4800063fb2267e64116a46fd723d6d6
PioKl <kosiu342@gmail.com>
2020-07-11 20:34:58 +0200

    Meme.js|Add new classes, memesCounter

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import '../style/Meme.scss'
 
-const Meme = ({ meme }) => {
+const Meme = ({ meme, memesCounter }) => {
 
     return (
         <div className="meme-card">
-            <img className="meme-card__img" src={meme.url} title={meme.name} alt={meme.name} />
+            <img className={`meme-card__img ${memesCounter <= 4 && "meme-card__img--lengthAdapted"}`} src={meme.url} title={meme.name} alt={meme.name} />
             <div className="meme-card__overlay">
                 <div className="meme-card__text">{meme.name}</div>
             </div>
