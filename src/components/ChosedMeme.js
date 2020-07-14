08001205b91ecc60bfa833b06375334ce8b74108
PioKl <kosiu342@gmail.com>
2020-07-14 19:45:24 +0200

    ChosedMeme.js|Add new classes

--- src/components/ChosedMeme.js
+++ src/components/ChosedMeme.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import '../style/ChosedMeme.scss';
+import { whiteSpacesReplace } from '../functions/replace';
 
 
 const ChosedMeme = ({ pickedMeme }) => {
@@ -69,7 +70,7 @@ const ChosedMeme = ({ pickedMeme }) => {
     return (
         <>
             {createdMeme ?
-                <div>
+                <div className="createdMeme">
                     <a href={createdMeme} rel="noopener noreferrer" target="_blank">
                         <img src={createdMeme} alt="" />
                     </a>
@@ -79,41 +80,46 @@ const ChosedMeme = ({ pickedMeme }) => {
                 <div className="chosedMeme__container">
                     <form className="chosedMeme__form-container" onSubmit={handleSubmit}>
                         <div className="chosedMeme__picture-container">
-                            <img className="chosedMeme__img" src={pickedMeme.url} /* width="500" height="500" */ alt="" />
+                            <img className={`chosedMeme__img chosedMeme__img--${whiteSpacesReplace(pickedMeme.name)}`} src={pickedMeme.url} /* width="500" height="500" */ alt="" />
                             {/* <h1 className="chosedMeme__title">{pickedMeme.name}</h1> */}
                         </div>
                         <div className="chosedMeme__creation-container">
+                            <div className="chosedMeme__title-container">
+                                <h1 className="chosedMeme__title">{pickedMeme.name}</h1>
+                            </div>
                             {/* <h1 className="chosedMeme__title">{pickedMeme.name}</h1> */}
-                            {pickedMeme.box_count <= 2 ?
-                                <>
-                                    <input className="chosedMeme__input" type="text" placeholder="top title" value={boxText0} onChange={e => { setBoxText0(e.target.value) }} />
-                                    <input className="chosedMeme__input" type="text" placeholder="bottom title" value={boxText1} onChange={e => { setBoxText1(e.target.value) }} />
-                                </>
-                                : pickedMeme.box_count <= 3 ?
+                            <div className="chosedMeme__textCreation-container">
+                                {pickedMeme.box_count <= 2 ?
                                     <>
-                                        <input className="chosedMeme__input" type="text" placeholder="text 1" value={boxText0} onChange={e => { setBoxText0(e.target.value) }} />
-                                        <input className="chosedMeme__input" type="text" placeholder="text 2" value={boxText1} onChange={e => { setBoxText1(e.target.value) }} />
-                                        <input className="chosedMeme__input" type="text" placeholder="text 3" value={boxText2} onChange={e => { setBoxText2(e.target.value) }} />
+                                        <input className="chosedMeme__input" type="text" placeholder="top title" value={boxText0} onChange={e => { setBoxText0(e.target.value) }} />
+                                        <input className="chosedMeme__input" type="text" placeholder="bottom title" value={boxText1} onChange={e => { setBoxText1(e.target.value) }} />
                                     </>
-                                    : pickedMeme.box_count <= 4 ?
+                                    : pickedMeme.box_count <= 3 ?
                                         <>
                                             <input className="chosedMeme__input" type="text" placeholder="text 1" value={boxText0} onChange={e => { setBoxText0(e.target.value) }} />
                                             <input className="chosedMeme__input" type="text" placeholder="text 2" value={boxText1} onChange={e => { setBoxText1(e.target.value) }} />
                                             <input className="chosedMeme__input" type="text" placeholder="text 3" value={boxText2} onChange={e => { setBoxText2(e.target.value) }} />
-                                            <input className="chosedMeme__input" type="text" placeholder="text 4" value={boxText3} onChange={e => { setBoxText3(e.target.value) }} />
                                         </>
-                                        :
-                                        <>
-                                            <input className="chosedMeme__input" type="text" placeholder="text 1" value={boxText0} onChange={e => { setBoxText0(e.target.value) }} />
-                                            <input className="chosedMeme__input" type="text" placeholder="text 2" value={boxText1} onChange={e => { setBoxText1(e.target.value) }} />
-                                            <input className="chosedMeme__input" type="text" placeholder="text 3" value={boxText2} onChange={e => { setBoxText2(e.target.value) }} />
-                                            <input className="chosedMeme__input" type="text" placeholder="text 4" value={boxText3} onChange={e => { setBoxText3(e.target.value) }} />
-                                            <input className="chosedMeme__input" type="text" placeholder="text 5" value={boxText4} onChange={e => { setBoxText4(e.target.value) }} />
-                                        </>
+                                        : pickedMeme.box_count <= 4 ?
+                                            <>
+                                                <input className="chosedMeme__input" type="text" placeholder="text 1" value={boxText0} onChange={e => { setBoxText0(e.target.value) }} />
+                                                <input className="chosedMeme__input" type="text" placeholder="text 2" value={boxText1} onChange={e => { setBoxText1(e.target.value) }} />
+                                                <input className="chosedMeme__input" type="text" placeholder="text 3" value={boxText2} onChange={e => { setBoxText2(e.target.value) }} />
+                                                <input className="chosedMeme__input" type="text" placeholder="text 4" value={boxText3} onChange={e => { setBoxText3(e.target.value) }} />
+                                            </>
+                                            :
+                                            <>
+                                                <input className="chosedMeme__input" type="text" placeholder="text 1" value={boxText0} onChange={e => { setBoxText0(e.target.value) }} />
+                                                <input className="chosedMeme__input" type="text" placeholder="text 2" value={boxText1} onChange={e => { setBoxText1(e.target.value) }} />
+                                                <input className="chosedMeme__input" type="text" placeholder="text 3" value={boxText2} onChange={e => { setBoxText2(e.target.value) }} />
+                                                <input className="chosedMeme__input" type="text" placeholder="text 4" value={boxText3} onChange={e => { setBoxText3(e.target.value) }} />
+                                                <input className="chosedMeme__input" type="text" placeholder="text 5" value={boxText4} onChange={e => { setBoxText4(e.target.value) }} />
+                                            </>
 
-                            }
+                                }
 
-                            <button className="chosedMeme__submitButton" type="submit">Create</button>
+                                <button className="chosedMeme__submitButton" type="submit">Create</button>
+                            </div>
                         </div>
                     </form>
                 </div>
