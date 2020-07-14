9a496871851b788672e32c1f80392994d9002947
PioKl <kosiu342@gmail.com>
2020-07-14 19:45:47 +0200

    Meme.js|Add new classes

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import '../style/Meme.scss'
+import { whiteSpacesReplace } from '../functions/replace';
 
 const Meme = ({ meme, memesCounter }) => {
 
     return (
         <div className="meme-card">
-            <img className={`meme-card__img ${memesCounter <= 4 && "meme-card__img--lengthAdapted"}`} src={meme.url} title={meme.name} alt={meme.name} />
+            <img className={`meme-card__img meme-card__img--${whiteSpacesReplace(meme.name)} ${memesCounter <= 4 && "meme-card__img--lengthAdapted"}`} src={meme.url} title={meme.name} alt={meme.name} />
             <div className="meme-card__overlay">
                 <div className="meme-card__text">{meme.name}</div>
             </div>
