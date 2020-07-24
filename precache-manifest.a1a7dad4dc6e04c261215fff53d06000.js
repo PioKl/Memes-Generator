879c3e11334634461926133f5ae4499f14f50dad
PioKl <kosiu342@gmail.com>
2020-07-24 16:40:35 +0200

    Updates

---
+++ precache-manifest.a1a7dad4dc6e04c261215fff53d06000.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "840dc732759956aa2050b8fce990acf8",
+    "url": "/Memes-Generator/index.html"
+  },
+  {
+    "revision": "22f78435efacaa12104e",
+    "url": "/Memes-Generator/static/css/main.68f6a22e.chunk.css"
+  },
+  {
+    "revision": "05959d3a91215790d39b",
+    "url": "/Memes-Generator/static/js/2.f41f0516.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Memes-Generator/static/js/2.f41f0516.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22f78435efacaa12104e",
+    "url": "/Memes-Generator/static/js/main.9fb1be60.chunk.js"
+  },
+  {
+    "revision": "b6931b857e3f3974004a",
+    "url": "/Memes-Generator/static/js/runtime-main.79eb904a.js"
+  }
+]);
