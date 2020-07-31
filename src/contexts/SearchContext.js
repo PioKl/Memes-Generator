fcf007e62517ed914f5e8d75bee51fe7d59c5d43
PioKl <kosiu342@gmail.com>
2020-07-31 17:18:29 +0200

    SearchContext.js|Add context for search to prevent bug with empty search (after user type something in input search for example "Futurama Fry", he will see that meme, then if he click that meme to create meme and he want back to previous site, then input search will be empty and he will see "Futurama Fry" meme, but input search shouldn't be empty, in input search should be "Futurama Fry" and context fixes it)

---
+++ src/contexts/SearchContext.js
@@ -0,0 +1,16 @@
+import React, { createContext, useState } from 'react';
+
+export const SearchContext = createContext();
+
+const SearchContextProvider = (props) => {
+    const [search, setSearch] = useState('')
+
+
+    return (
+        <SearchContext.Provider value={{ search, setSearch }}>
+            {props.children}
+        </SearchContext.Provider>
+    )
+}
+
+export default SearchContextProvider;
